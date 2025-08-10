fdfed116531026690f1f0cd61ed1c6c511988e43
yysum101 <144432395+yysum101@users.noreply.github.com>
2025-08-10 11:15:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -495,8 +495,9 @@ def chat():
         user=user,
     )
 
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
+
